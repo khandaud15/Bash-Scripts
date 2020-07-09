41e596b8a20814e63110205c0adcad16bb8956ab
khandaud15 <32276275+khandaud15@users.noreply.github.com>
2020-07-09 13:00:47 -0400

    Update submit03_samsort.sh

--- submit03_samsort.sh
+++ submit03_samsort.sh
@@ -2,10 +2,9 @@
 #PBS -S /bin/bash
 #PBS -V
 #PBS -l nodes=1:ppn=24
-#PBS -M mdozmorov@vcu.edu
 #PBS -N mdozsamsort
 #PBS -j oe
-# PBS -o /home/glasser/Dissertation/Dozmorov/RNA-seq/cuffdiffout3
+# PBS -o /home/glasser/Dissertation/RNA-seq/cuffdiffout3
 
 cd $PBS_O_WORKDIR
 
@@ -15,9 +14,9 @@ mkdir 03_sorted
 
 
 for file in `find 02_subread-align/ -type f -name "*.bam" | sort`; do
-	/home/mdozmorov/.local/bin/samtools sort -@ 23 -o 03_sorted/$file -O BAM $file;
+	/local/bin/samtools sort -@ 23 -o 03_sorted/$file -O BAM $file;
 done
 
 for file in `find 03_sorted/ -type f -name "*.bam" | sort`; do
-	/home/mdozmorov/.local/bin/samtools index $file;
+	/local/bin/samtools index $file;
 done
